cc92a183ebd32260fff4388a2b6980c7c0a03c0c
donny <donny526@outlook.com>
2023-08-29 15:08:46 +0800

    fix: unocss config error

--- uno.config.ts
+++ uno.config.ts
@@ -7,8 +7,12 @@ import {
 } from 'unocss'
 
 export default defineConfig({
-  include: ['**.ts', '**.tsx', '**.vue'],
-  exclude: ['node_modules', '.git'],
+  content: {
+    pipeline: {
+      include: ['**.ts', '**.tsx', '**.vue'],
+      exclude: ['node_modules', '.git'],
+    },
+  },
   shortcuts: {
     'wh-full': 'w-full h-full',
     'flex-center': 'flex justify-center items-center',
