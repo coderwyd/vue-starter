3139ca44835bdcbe7384daf31b7b202f93eb7151
donny <donny526@outlook.com>
2023-08-29 15:36:14 +0800

    fix: ci

--- src/settings/mapSetting.ts
+++
@@ -1,7 +0,0 @@
-import type { LoaderOptions } from '@googlemaps/js-api-loader'
-
-export const mapBasicConfig: LoaderOptions = {
-  apiKey: 'AIzaSyCVn1mEVk0KZjtvjZv8pwpt2Qi2v3GzHWk',
-  language: 'en',
-  libraries: ['places'],
-}
